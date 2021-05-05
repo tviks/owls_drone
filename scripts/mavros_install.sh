41e501f5c8d60572f1f686f4508febfe08e9d923
Sergey Berstnev <tviks879@gmail.com>
2021-05-05 04:43:49 +0300

    Add files via upload

---
+++ scripts/mavros_install.sh
@@ -0,0 +1,5 @@
+sudo apt-get install ros-melodic-mavros ros-melodic-mavros-extras
+
+wget https://raw.githubusercontent.com/mavlink/mavros/master/mavros/scripts/install_geographiclib_datasets.sh
+
+sudo bash ./install_geographiclib_datasets.sh   
